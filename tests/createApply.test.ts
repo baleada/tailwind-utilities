d1f9dc473f66f9f462fd7e654aca9b8c92a5c6af
Alex Vipond <alex.vipond@betterhelp.com>
2023-02-12 16:25:28 -0600

    support prefixes, support fixed and sticky

---
+++ tests/createApply.test.ts
@@ -0,0 +1,33 @@
+import { suite as createSuite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { createApply } from '../src/'
+
+const suite = createSuite('createApply')
+
+suite(`Applies classes with no prefix`, () => {
+  const apply = createApply('')
+  assert.equal(apply('w-full h-full'), {
+    '@apply w-full h-full': {}
+  })
+})
+
+suite('Applies classes with a prefix', () => {
+  const apply = createApply('tw-')
+  assert.equal(apply('w-full h-full'), {
+    '@apply tw-w-full tw-h-full': {}
+  })
+})
+
+suite('Handles excessive spaces and newlines', () => {
+  const apply = createApply('tw-')
+  assert.equal(apply(`w-full  
+  
+  
+  h-full
+
+  `), {
+    '@apply tw-w-full tw-h-full': {}
+  })
+})
+
+suite.run()
